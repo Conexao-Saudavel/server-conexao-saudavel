747e1dc0e808acbeccfa6fbb934d5feeafa6a0c7
Francisco Macedo <macedo270701@gmail.com>
2025-05-23 23:46:27 -0300

    feat: Add Role entity with properties and relationships

---
+++ src/entities/Role.ts
@@ -0,0 +1,30 @@
+import { Entity, PrimaryGeneratedColumn, Column, ManyToMany, JoinTable } from 'typeorm';
+
+@Entity('roles')
+export class Role {
+  @PrimaryGeneratedColumn('uuid')
+  id!: string;
+
+  @Column({ unique: true })
+  name!: string;
+
+  @Column()
+  description!: string;
+
+  @ManyToMany('User', 'roles')
+  users!: any[];
+
+  @ManyToMany('Permission', 'roles')
+  @JoinTable({
+    name: 'role_permissions',
+    joinColumn: { name: 'role_id', referencedColumnName: 'id' },
+    inverseJoinColumn: { name: 'permission_id', referencedColumnName: 'id' },
+  })
+  permissions!: any[];
+
+  @Column({ type: 'timestamp', default: () => 'CURRENT_TIMESTAMP' })
+  created_at!: Date;
+
+  @Column({ type: 'timestamp', default: () => 'CURRENT_TIMESTAMP', onUpdate: 'CURRENT_TIMESTAMP' })
+  updated_at!: Date;
+} 
