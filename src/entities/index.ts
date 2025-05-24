7768cbc47c5d86173715911e611d3680162775fe
Francisco Macedo <macedo270701@gmail.com>
2025-05-23 23:46:44 -0300

    feat: Export all entities from index file for improved module accessibility

--- src/entities/index.ts
+++ src/entities/index.ts
@@ -1,4 +1,8 @@
-export { User } from './User.js';
-export { Institution } from './Institution.js';
-export { Device } from './Device.js';
-export { UserSettings } from './UserSettings.js'; 
+export * from './User.js';
+export * from './Device.js';
+export * from './UserSettings.js';
+export * from './Institution.js';
+export * from './DailySummary.js';
+export * from './AppUsage.js';
+export * from './Role.js';
+export * from './Permission.js'; 
